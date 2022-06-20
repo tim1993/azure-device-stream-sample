62dd68ffcbbbf5c31dac483c9492a7c081fc755d
Dominik Deschner <deschner@medialesson.de>
2022-06-20 21:05:55 +0200

    Adds razor project

---
+++ src/IoTEmergency.Web/Data/DeviceStreamProxyService.cs
@@ -0,0 +1,116 @@
+﻿using Microsoft.Azure.Devices;
+using System.Net;
+using System.Net.Sockets;
+using System.Net.WebSockets;
+
+namespace IoTEmergency.Web.Data
+{
+    public class DeviceStreamProxyService
+    {
+        private readonly ServiceClient deviceClient;
+        private Task? proxyTask;
+        private CancellationTokenSource cts = new CancellationTokenSource();
+        public DeviceStreamProxyService(ServiceClient deviceClient)
+        {
+            this.deviceClient = deviceClient;
+        }
+
+        private static async Task HandleIncomingDataAsync(NetworkStream localStream, ClientWebSocket remoteStream, CancellationToken cancellationToken)
+        {
+            byte[] receiveBuffer = new byte[10240];
+
+            while (localStream.CanRead)
+            {
+                var receiveResult = await remoteStream.ReceiveAsync(receiveBuffer, cancellationToken).ConfigureAwait(false);
+
+                await localStream.WriteAsync(receiveBuffer, 0, receiveResult.Count).ConfigureAwait(false);
+            }
+        }
+
+        private static async Task HandleOutgoingDataAsync(NetworkStream localStream, ClientWebSocket remoteStream, CancellationToken cancellationToken)
+        {
+            byte[] buffer = new byte[10240];
+
+            while (remoteStream.State == WebSocketState.Open)
+            {
+                int receiveCount = await localStream.ReadAsync(buffer, 0, buffer.Length).ConfigureAwait(false);
+
+                await remoteStream.SendAsync(new ArraySegment<byte>(buffer, 0, receiveCount), WebSocketMessageType.Binary, true, cancellationToken).ConfigureAwait(false);
+            }
+        }
+
+        private static async Task HandleIncomingConnectionsAndCreateStreams(string deviceId, ServiceClient serviceClient, NetworkStream tcpClient)
+        {
+            DeviceStreamRequest deviceStreamRequest = new DeviceStreamRequest(
+                streamName: "TestStream"
+            );
+
+
+            DeviceStreamResponse result = await serviceClient.CreateStreamAsync(deviceId, deviceStreamRequest, CancellationToken.None).ConfigureAwait(false);
+
+            Console.WriteLine($"Stream response received: Name={deviceStreamRequest.StreamName} IsAccepted={result.IsAccepted}");
+
+            if (result.IsAccepted)
+            {
+                try
+                {
+                    var cancellationTokenSource = new CancellationTokenSource();
+                    using (var remoteStream = await GetStreamingClientAsync(result.Uri, result.AuthorizationToken, cancellationTokenSource.Token).ConfigureAwait(false))
+                    {
+                        Console.WriteLine("Starting streaming");
+
+                        await Task.WhenAny(
+                            HandleIncomingDataAsync(tcpClient, remoteStream, cancellationTokenSource.Token),
+                            HandleOutgoingDataAsync(tcpClient, remoteStream, cancellationTokenSource.Token)).ConfigureAwait(false);
+                    }
+
+                    Console.WriteLine("Done streaming");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Got an exception: {0}", ex);
+                }
+            }
+            tcpClient.Close();
+        }
+
+        private static async Task<ClientWebSocket> GetStreamingClientAsync(Uri url, string authorizationToken, CancellationToken cancellationToken)
+        {
+            ClientWebSocket wsClient = new ClientWebSocket();
+            wsClient.Options.SetRequestHeader("Authorization", "Bearer " + authorizationToken);
+
+            await wsClient.ConnectAsync(url, cancellationToken).ConfigureAwait(false);
+
+            return wsClient;
+        }
+
+        public async Task OpenLocalProxy(int localPort, string deviceId)
+        {
+            if (proxyTask is not null)
+            {
+                cts.Cancel();
+                await proxyTask.WaitAsync(TimeSpan.FromSeconds(10));
+                proxyTask.Dispose();
+
+                cts = new CancellationTokenSource();
+            }
+            proxyTask = Task.Run(async () =>
+            {
+                var tcpListener = new TcpListener(IPAddress.Any, localPort);
+                tcpListener.Start();
+
+                while (!cts.IsCancellationRequested)
+                {
+                    using var tcpClient = await tcpListener.AcceptSocketAsync().ConfigureAwait(false);
+                    using var networkStream = new NetworkStream(tcpClient);
+                    await HandleIncomingConnectionsAndCreateStreams(deviceId, deviceClient, networkStream);
+                }
+
+                tcpListener.Stop();
+            });
+
+        }
+
+
+    }
+}
---
+++ src/IoTEmergency.Web/Program.cs
@@ -0,0 +1,34 @@
+using IoTEmergency.Web.Data;
+using Microsoft.Azure.Devices;
+
+var ServiceConnectionString = File.ReadAllText(".key");
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<IoTEmergencyRoomService>();
+builder.Services.AddSingleton<DeviceStreamProxyService>();
+
+builder.Services.AddSingleton(_ => ServiceClient.CreateFromConnectionString(ServiceConnectionString));
+builder.Services.AddSingleton(_ => RegistryManager.CreateFromConnectionString(ServiceConnectionString));
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
